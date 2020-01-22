da13b49e2d037bbb8e1bbf7b99cd0368f68d4b68
George Gruner <imgeorge220@gmail.com>
2020-01-22 12:11:10 -0800

    morning finished

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,41 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import functions
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def add_num():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return f"<h1>{functions['add'](a, b)}</h1>"
+
+
+@app.route('/sub')
+def sub_num():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return f"<h1>{functions['sub'](a, b)}</h1>"
+
+
+@app.route('/mult')
+def mult_num():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return f"<h1>{functions['mult'](a, b)}</h1>"
+
+
+@app.route('/div')
+def div_num():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return f"<h1>{functions['div'](a, b)}</h1>"
+
+
+@app.route('/math/<function>')
+def math(function):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    print(functions.get(function))
+    function_passed = functions.get(function)
+    return f"<h1>{function_passed(a, b)}</h1>"
--- flask-greet-calc/calc/operations.py
+++ flask-greet-calc/calc/operations.py
@@ -1,21 +1,33 @@
 """Basic math operations."""
 
+
 def add(a, b):
     """Add a and b."""
-    
+
     return a + b
+
 
 def sub(a, b):
     """Substract b from a."""
 
     return a - b
 
+
 def mult(a, b):
     """Multiply a and b."""
 
     return a * b
 
+
 def div(a, b):
     """Divide a by b."""
 
     return a / b
+
+
+functions = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div
+}
